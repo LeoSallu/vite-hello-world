e6458d4f8cabc3510de0eb82b5104c9fbae95dbe
LeoSallu <L.sallustio99@gmail.com>
2023-03-22 16:15:37 +0100

    Es base completato

---
+++ vite-project/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import appTitle from './components/appTitle.vue';
+export default{
+  components:{
+    appTitle
+  }
+}
+</script>
+<template>
+  <appTitle></appTitle>
+</template>
---
+++ vite-project/src/components/appTitle.vue
@@ -0,0 +1,12 @@
+<script>
+export default{
+  data(){
+    return{
+      title: 'La mia prima App con Vite'
+    }
+  }
+}
+</script>
+<template>
+  <h1>{{title}}</h1>
+</template>
